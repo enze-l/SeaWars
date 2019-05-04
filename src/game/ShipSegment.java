79bcfab33ab703816147ca7a9af3a45c78571a17
zink-l <zink-l@users.noreply.github.com>
2019-05-04 18:36:32 +0200

    added jdoc for segment

--- src/game/ShipSegment.java
+++ src/game/ShipSegment.java
@@ -7,23 +7,40 @@ class ShipSegment {
     private FieldStatus status;
     private Coordinate coordinate;
 
+    /**
+     * a ship-segment contains information about its place on the board, as well as it's status.
+     * Its default status is "Ship".
+     * @param coordinate is the place of the segment on the board
+     */
     ShipSegment(Coordinate coordinate){
         this.coordinate=coordinate;
         this.status=FieldStatus.SHIP;
     }
 
+    /**
+     * @return placement of segment on the board
+     */
     Coordinate getCoordinate(){
         return this.coordinate;
     }
 
+    /**
+     * @return status of the segment
+     */
     FieldStatus getStatus(){
         return this.status;
     }
 
+    /**
+     * changes status of segment to "Hit"
+     */
     void setHit(){
         this.status=FieldStatus.HIT;
     }
 
+    /**
+     * changes status of segment to "Sunk"
+     */
     void setSunk(){
         this.status=FieldStatus.SUNK;
     }
