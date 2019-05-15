1d5fe48d59e9dad294c1b1ffa583c50a8b1390b0
zink-l <zink-l@users.noreply.github.com>
2019-05-15 22:07:17 +0200

    Added Output-Class. Assigned FieldStatus different symbols.

---
+++ src/game/OutputImpl.java
@@ -0,0 +1,26 @@
+package game;
+
+/**
+ * @author s0568823 - Leon Enzenberger
+ */
+public class OutputImpl implements Output {
+    @Override
+    public void outputMyBoard() {
+
+    }
+
+    @Override
+    public void outputEnemyBoard() {
+
+    }
+
+    @Override
+    public void outputShips() {
+
+    }
+
+    @Override
+    public void outputCommandsAvailable() {
+
+    }
+}
---
+++ src/game/OutputSymbols.java
@@ -0,0 +1,28 @@
+package game;
+
+/**
+ * @author s0568823 - Leon Enzenberger
+ */
+public class OutputSymbols {
+    static char getSymbol(FieldStatus fieldStatus){
+        char fieldSymbol=' ';
+        switch (fieldStatus){
+            case WATER:
+                fieldSymbol = '░';
+                break;
+            case SHOTWATER:
+                fieldSymbol = '◦';
+                break;
+            case SHIP:
+                fieldSymbol = '■';
+                break;
+            case HIT:
+                fieldSymbol = '◙';
+                break;
+            case SUNK:
+                fieldSymbol = '□';
+                break;
+        }
+        return fieldSymbol;
+    }
+}
